99f9296f8c45f92a10a436e9dbb6b18890fabe9e
Gladkowski <vadim.gladkovskiy@gmail.com>
2018-11-27 01:29:41 +0100

    [dev-task9] - scrollview added

--- app/src/main/java/dev/gladkowski/wetaherapp/presentation/main/MainActivity.java
+++ app/src/main/java/dev/gladkowski/wetaherapp/presentation/main/MainActivity.java
@@ -3,7 +3,6 @@ package dev.gladkowski.wetaherapp.presentation.main;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
@@ -12,8 +11,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 
 import com.arellomobile.mvp.presenter.InjectPresenter;
 import com.arellomobile.mvp.presenter.ProvidePresenter;
@@ -62,11 +59,10 @@ public class MainActivity extends BaseActivity<MainPresenter, MainView>
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
         ButterKnife.bind(this);
 
-        // Makes the status bar transparent
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Window w = getWindow();
-            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        }
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//            Window w = getWindow();
+//            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+//        }
     }
 
     ///////////////////////////////////////////////////////////////////////////
